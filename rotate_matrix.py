95a963deef1896da557a0740ca72e79cf8d3cc53
gbstorey <gbstorey@asu.edu>
2022-10-28 13:37:00 -0700

    solve rotate matrix

--- rotate_matrix.py
+++ rotate_matrix.py
@@ -1,20 +1,19 @@
 
 myMatrix = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9]
+    [1,2,3,4],
+    [5,6,7,8],
+    [9,10,11,12],
+    [13,14,15,16]
 ]
 
 def rotate_matrix(myMatrix):
-    newMatrix = [
-        [],
-        [],
-        []
-    ]
+    newMatrix = []
     numRows = len(myMatrix)
     for n in range(numRows):
+        newMatrix.append([])
+    for n in range(numRows):
         for i in range(numRows):
-            newMatrix[i].append(myMatrix[numRows-n][i])
+            newMatrix[i].append(myMatrix[numRows-1-n][i])
     return newMatrix
 
 print(rotate_matrix(myMatrix))
